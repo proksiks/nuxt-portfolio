f503325e789274e2cb6ddbdeef41c9a24aef8388
Volodya <proksik49032@mail.ru>
2021-05-25 21:03:35 +0400

    change nuxt config

---
+++ components/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>PQOEWPQEQW</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -2,10 +2,6 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
-  router: {
-    base: '/nuxt-portfolio/'
-  },
-
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'app',
