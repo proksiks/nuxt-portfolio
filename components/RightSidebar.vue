f6296badcbf84977b4051b18c6634806f622a889
khliupnevva <v.khliupnev@haulmont.com>
2021-06-03 17:09:01 +0400

    first screen add links

--- components/RightSidebar.vue
+++ components/RightSidebar.vue
@@ -2,12 +2,17 @@
   <div class="right-sidebar">
     <div class="right-sidebar__wrapper">
       <ul class="right-sidebar__icons socials">
-        <li v-for="icon in fields.socials" :key="icon.name" class="right-sidebar__icon socials-icon" >
+        <li
+          v-for="icon in fields.socials"
+          :key="icon.name"
+          class="right-sidebar__icon socials-icon"
+        >
           <a :href="icon.link">
             <Icon :icon="icon.name" :className="icon.name" />
           </a>
         </li>
       </ul>
+      <div class="right-sidebar__copiryght">© Copyright reserved</div>
     </div>
   </div>
 </template>
--- components/pages/index/IndexPage.vue
+++ components/pages/index/IndexPage.vue
@@ -2,7 +2,7 @@
   <div class="content-wrapper">
     <swiper ref="mySwiper" :options="swiperOptions">
       <swiper-slide :key="slide.url" v-for="slide in fields.slides">
-        <a :href="slide.link" class="slide-wrap">
+        <a :href="slide.link" class="slide-wrap" target="_blank">
           <img :src="require(`~/assets/images/main/${slide.url}`)" alt="" />
           <div class="slide-wrap__link">
             <div class="slide-wrap__text">
@@ -83,62 +83,6 @@ export default {
 };
 </script>
 <style lang="scss" >
-.content-wrapper {
-  height: 100vh;
-  padding: 0 100px;
-  @media (max-width: 970px) {
-    padding: 0 50px 0 100px;
-  }
-  @media (max-width: 900px) {
-    padding: 0 49px;
-  }
-  @media (max-width: 768px) {
-    padding: 0;
-  }
-}
-.main-footer {
-  padding: 34px 0;
-  background-color: #1c1e25;
-  @media (max-width: 400px) {
-    padding: 10px 0;
-  }
-  &__wrap {
-    color: #fff;
-    letter-spacing: 6px;
-    padding: 0 90px;
-    text-transform: uppercase;
-    font-size: 12px;
-    line-height: 1;
-    font-weight: 600;
-    @media (max-width: 768px) {
-      padding: 0 40px;
-    }
-  }
-  &__left {
-    p {
-      position: relative;
-      span {
-        color: #cc9b55;
-      }
-      &::before {
-        content: "";
-        position: absolute;
-        left: -30px;
-        top: 42%;
-        transform: translateY(-50%);
-        width: 20px;
-        height: 1px;
-        background-color: #cc9b55;
-        @media (max-width: 400px) {
-          display: none;
-        }
-      }
-    }
-    @media (max-width: 400px) {
-      text-align: center;
-    }
-  }
-}
 .swiper-container {
   overflow: visible;
 }
@@ -154,125 +98,6 @@ export default {
     display: none;
   }
 }
-
-.slide-wrap {
-  position: relative;
-  display: block;
-  overflow: hidden;
-  @media (max-width: 400px) {
-    margin-bottom: -1px;
-  }
-  &:hover {
-    img {
-      transform: scale(1.2);
-    }
-    &::before {
-      background-color: rgba(0, 0, 0, 0.3);
-    }
-    .slide-wrap__text {
-      .square {
-        &::before {
-          left: 10px;
-        }
-      }
-    }
-  }
-  &:before {
-    content: "";
-    position: absolute;
-    left: 0;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    width: 100%;
-    height: 100%;
-    z-index: 2;
-    transition: all 0.5s ease-out;
-    background-color: rgba(0, 0, 0, 0.1);
-  }
-  &__link {
-    position: absolute;
-    left: 0;
-    bottom: 0;
-    width: 100%;
-    z-index: 3;
-    font-size: 26px;
-    line-height: 1;
-    transition: all 0.5s ease-out;
-    font-family: "Barlow Condensed", sans-serif;
-  }
-  &__text {
-    position: relative;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    margin: 60px;
-    @media (max-width: 400px) {
-      margin: 20px;
-      justify-content: center;
-    }
-    p {
-      transition: color 0.5s ease-out;
-    }
-    &:hover {
-      p {
-        color: #cc9b55;
-      }
-    }
-    .square {
-      position: relative;
-      width: 20px;
-      height: 20px;
-      border: 1px solid #cc9b55;
-      &::before {
-        content: "";
-        position: absolute;
-        top: 10px;
-        left: -10px;
-        width: 100%;
-        height: 1px;
-        background-color: #cc9b55;
-        transition: all 0.5s ease-out;
-      }
-      @media (max-width: 800px) {
-        display: none;
-      }
-    }
-  }
-}
-
-.swiper-slide {
-  img {
-    display: block;
-    width: 100%;
-    height: calc(100vh - 80px);
-    object-fit: cover;
-    transition: transform 3s ease;
-    @media (max-width: 400px) {
-      height: calc(100vh - 30px);
-    }
-  }
-}
-.swiper-pagination-fraction,
-.swiper-pagination-custom,
-.swiper-container-horizontal > .swiper-pagination-bullets {
-  text-align: right;
-  left: auto;
-  top: auto;
-  right: 115px;
-  bottom: -55px;
-  width: auto;
-  @media (max-width: 1300px) {
-    right: 50px;
-  }
-  @media (max-width: 768px) {
-    right: 20px;
-  }
-  @media (max-width: 400px) {
-    bottom: -30px;
-  }
-}
-
 .swiper-pagination-bullet {
   position: relative;
   opacity: 1;
@@ -299,6 +124,25 @@ export default {
 .swiper-pagination-bullet-active {
   &::before {
     background: #cc9b55;
+  }
+}
+.swiper-pagination-fraction,
+.swiper-pagination-custom,
+.swiper-container-horizontal > .swiper-pagination-bullets {
+  text-align: right;
+  left: auto;
+  top: auto;
+  right: 115px;
+  bottom: -55px;
+  width: auto;
+  @media (max-width: 1300px) {
+    right: 50px;
+  }
+  @media (max-width: 768px) {
+    right: 20px;
+  }
+  @media (max-width: 400px) {
+    bottom: -30px;
   }
 }
 </style>
