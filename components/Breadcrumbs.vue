0584faea11444eb857e5035402eac504f7edd74b
khliupnevva <v.khliupnev@haulmont.com>
2021-06-16 18:07:14 +0400

    about-page

---
+++ components/Breadcrumbs.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="breadcrumbs">
+    <div class="breadcrumbs__decorate"></div>
+    <div class="breadcrumbs__wrap">
+      <div class="breadcrumbs__links">
+        <nuxt-link to="/" class="breadcrumbs__link breadcrumbs__link_home">HOME</nuxt-link>
+        <nuxt-link :to="href" class="breadcrumbs__link breadcrumbs__link_subtitle">{{ title }}</nuxt-link>
+      </div>
+      <h2 class="breadcrumbs__title">{{ title }}</h2>
+    </div>
+    <div class="breadcrumbs__prompt">
+      scroll down
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Breadcrumbs",
+  props: {
+    title: {
+      type: String,
+      default: null,
+    },
+    href: String,
+  },
+};
+</script>
+
+
+<style scoped>
+</style>
--- pages/about.vue
+++ pages/about.vue
@@ -1,12 +1,43 @@
 <template>
-  <h3>21</h3>
+  <section class="container">
+    <Breadcrumbs href="/about" title="About" />
+    <div class="about">
+      <div class="about__hero">
+        <div class="about__image">
+          <img src="" alt="" />
+          <div class="about__text">
+            Lodiz is trusted by more than 8,700 customers
+          </div>
+        </div>
+        <div class="subtitle">About Our Company</div>
+        <div class="title">
+          It just might inspire you to help us build a better future
+        </div>
+        <div class="text">
+          Lorem ipsum dolor sit amet nsectetur cing elit. Suspe ndisse suscipit
+          sagittis leo sit met entum is not estibulum dignissim posuere cubilia
+          durae. Leo sit met entum cubilia crae. but also the leap into
+          electronic typesetting, remaining essentially unchanged. It was
+          popularised in the 1960s with the release of Letraset sheets
+          containing Lorem Ipsum passages and more recently.
+        </div>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
+import Breadcrumbs from "~/components/Breadcrumbs";
 export default {
-}
+  name: "about",
+  components: {
+    Breadcrumbs,
+  },
+};
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+h3 {
+  text-align: center;
+}
 </style>
