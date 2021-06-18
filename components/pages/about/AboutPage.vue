4de4e5920e00a78a8ae35c6f59469fb18359abb7
khliupnevva <v.khliupnev@haulmont.com>
2021-06-18 17:27:35 +0400

    about page

--- components/pages/about/AboutPage.vue
+++ components/pages/about/AboutPage.vue
@@ -2,20 +2,29 @@
   <div class="about">
     <div class="about__hero">
       <div class="about__image">
-        <img :src="require(`~/assets/images/about/${fields.top.image}`)" alt="" />
-        <div class="about__text text">{{ fields.top.imageDescr }}</div>
+        <img
+          :src="require(`~/assets/images/about/${fields.top.image}`)"
+          alt=""
+        />
+        <p class="about__text text">{{ fields.top.imageDescr }}</p>
       </div>
-      <div class="about__subtitle subtitle">{{fields.top.subtitle}}</div>
-      <div class="about__title title">{{fields.top.title}}</div>
-      <div class="about__text text">{{fields.top.descr}}</div>
+      <h4 class="about__subtitle subtitle">{{ fields.top.subtitle }}</h4>
+      <h2 class="about__title title">{{ fields.top.title }}</h2>
+      <p class="about__text text">{{ fields.top.descr }}</p>
     </div>
     <div class="about__team">
-      <div class="about__subtitle subtitle">our experts</div>
-      <div class="about__title title">Meet the team</div>
+      <h4 class="about__subtitle subtitle">
+        {{ fields.aboutTeams.subtitle }}
+      </h4>
+      <h2 class="about__title title">{{ fields.aboutTeams.title }}</h2>
       <div class="about__teams">
-        <div class="about__team" v-for="team in fields.aboutTeams.teams" :key="team.id">
+        <div
+          class="about__team"
+          v-for="team in fields.aboutTeams.teams"
+          :key="team.id"
+        >
           <div class="about__team-img">
-            <img src="#" alt="" />
+            <img :src="require(`~/assets/images/about/${team.photo}`)" alt="" />
           </div>
           <div class="about__team-name name">{{ team.name }}</div>
           <div class="about__team-position position">{{ team.position }}</div>
@@ -32,21 +41,13 @@
       </div>
     </div>
     <div class="about__stats">
-      <div class="about__counter counter">
-        <span class="counter__number">990</span>
-        <span class="counter__text">happy customers</span>
-      </div>
-      <div class="about__counter counter">
-        <span class="counter__number">16</span>
-        <span class="counter__text">awards won</span>
-      </div>
-      <div class="about__counter counter">
-        <span class="counter__number">30</span>
-        <span class="counter__text">years experience</span>
-      </div>
-      <div class="about__counter counter">
-        <span class="counter__number">247</span>
-        <span class="counter__text">projects completed</span>
+      <div
+        class="about__counter counter"
+        v-for="counter in fields.aboutStats"
+        :key="counter.id"
+      >
+        <p class="counter__number">{{ counter.number }}</p>
+        <p class="counter__text subtitle-grey">{{ counter.text }}</p>
       </div>
     </div>
   </div>
