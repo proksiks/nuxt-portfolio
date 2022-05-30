ed1f237847d116104640b460faca532f787fc436
Vladimir Khliupnev <v.khliupnev@haulmont.com>
2022-05-30 22:11:27 +0400

    098e700

---
+++ _nuxt/static/1653934268/about/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fnuxt-portfolio\u002F_nuxt\u002Fstatic\u002F1653934268",layout:"default",error:a,serverRendered:true,routePath:"\u002Fabout",config:{_app:{basePath:"\u002Fnuxt-portfolio\u002F",assetsPath:"\u002Fnuxt-portfolio\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"2c70a2b1"}}}}(null));
